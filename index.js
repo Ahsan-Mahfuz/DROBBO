03af793d6be4da0adf6e8917e39d72dce4ea78f8
Ahsan Mahfuz <79737104+Ahsan-Mahfuz@users.noreply.github.com>
2024-11-19 08:51:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '50mb' })) // You can adjust the size as needed
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(
   cors({
-    origin: 'http://localhost:5173', // React frontend URL
+    origin: 'https://drobbo.netlify.app', // React frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
